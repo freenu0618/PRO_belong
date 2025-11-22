72aba59afc99a99fedd5517af709fd1024afd4e2
freenu0618 <freenu0618@naver.com>
2025-11-22 14:41:47 +0900

    docs : prepocess.py(ml), web.api.__init__.py, web.api.routes.py, web.__init__.py

---
+++ belong/web/api/__init__.py
@@ -0,0 +1,7 @@
+from flask import Blueprint
+
+api_bp = Blueprint('api', __name__)
+
+from . import routes # noqa
+# Blueprint는 import 되는 순간 라우트가 등록됨
+# 그래서 from . import routes 가 실행되어야  api_bp에 연결된 라우트들이 완성됨
