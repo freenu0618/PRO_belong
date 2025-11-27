49b2e9f52c186208418629983e8e31a33e259688
freenu0618 <freenu0618@naver.com>
2025-11-27 10:34:38 +0900

    디테ì메인화면 디테일 추가 완료

--- belong/models/__init__.py
+++ belong/models/__init__.py
@@ -1,8 +1,8 @@
 from .region import Region
 from .feature_stats import ElderlyStats
-# from .elderly_population import ElderlyHistory
+from .elderly_population import ElderlyHistory
 
 __all__ = [
-    "Region",
+    # "Region",
     "ElderlyStats",
 ]
