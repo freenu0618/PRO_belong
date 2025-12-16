67225e7acbb4f5f1c5aaae7f9c4c9ada74f27e01
freenu0618 <freenu0618@naver.com>
2025-12-16 11:29:12 +0900

    UI 수정: 네비게이션바 중복 노출 문제 해결을 위한 layout.html 삭제

--- run.py
+++ run.py
@@ -1,1 +1,7 @@
+from belong.app import create_app
 
+app = create_app()
+
+if __name__ == "__main__":
+    # 0.0.0.0으로 해야 도커 컨테이너 외부(내 PC)에서 접속 가능합니다.
+    app.run(host="0.0.0.0", port=5000, debug=True)
